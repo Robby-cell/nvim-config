2e167d6d6635120fff52f8d14c587e99e77e35ff
Robert Williamson <robert11122002@gmail.com>
2023-07-10 18:36:20 +0100

    added indent binds

--- lua/astronvim/mappings.lua
+++ lua/astronvim/mappings.lua
@@ -23,6 +23,9 @@ local sections = {
 maps.n["<leader>bn"] = { "<cmd>:bnext<cr>", desc = "Moves buffer right" }
 maps.n["<leader>bb"] = { "<cmd>:bprev<cr>", desc = "Moves buffer left" }
 
+maps.n["<leader>["] = { "V<", desc = "Removes indent" }
+maps.n["<leader>]"] = { "V>", desc = "Adds indent" }
+
 -- Normal --
 -- Standard Operations
 maps.n["j"] = { "v:count == 0 ? 'gj' : 'j'", expr = true, desc = "Move cursor down" }
